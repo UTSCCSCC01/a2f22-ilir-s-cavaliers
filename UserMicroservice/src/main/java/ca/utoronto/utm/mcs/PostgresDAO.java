43f2d5094e84a4b4d0ea32072cfe0e7ca0f70c75
Kia Rahmati <keia.r.ahmati@gmail.com>
2022-11-23 01:38:11 -0500

    feature/userMicroService: made: login & register

--- UserMicroservice/src/main/java/ca/utoronto/utm/mcs/PostgresDAO.java
+++ UserMicroservice/src/main/java/ca/utoronto/utm/mcs/PostgresDAO.java
@@ -36,6 +36,33 @@ public class PostgresDAO {
         return this.st.executeQuery(query);
     }
 
+    public ResultSet getUserDataFromEmail(String email) throws SQLException {
+        String query = "SELECT uid, prefer_name as name, password, email, rides, isdriver"
+            + " FROM users WHERE email = '%s'";
+        query = String.format(query, email);
+        return this.st.executeQuery(query);
+    }
+
+    //I scratched my head for a while on how to make a unique ID but i rubbed my two brain cells together and came up with something
+    //UID = sum of all current ID's, it's not pretty but it should be unique
+    public int getNewUID() throws SQLException{
+        String query = "SELECT SUM(uid) FROM USERS";
+        ResultSet rs = this.st.executeQuery(query);
+        rs.next();
+        return rs.getInt("sum");
+    }
+
+    public int registerUser(String name, String email, String password) throws  SQLException {
+        String query = "INSERT INTO users(uid, password, email, prefer_name, rides, isdriver)"
+            + " VALUES('%s', '%s', '%s', '%s', 0, false)";
+        query = String.format(query, getNewUID(), password, email, name);
+        this.st.execute(query);
+        ResultSet rs = getUserDataFromEmail(email);
+        rs.next();
+        int uid = rs.getInt("uid");
+        return uid;
+    }
+
     public void updateUserAttributes(int uid, String email, String password, String prefer_name, Integer rides, Boolean isDriver) throws SQLException {
 
         String query;
--- UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
+++ UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
@@ -2,7 +2,10 @@ package ca.utoronto.utm.mcs;
 
 import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.sql.ResultSet;
 
 public class Register extends Endpoint {
 
@@ -15,6 +18,65 @@ public class Register extends Endpoint {
 
     @Override
     public void handlePost(HttpExchange r) throws IOException, JSONException {
-        // TODO
+         // check if request url isn't malformed
+		String[] splitUrl = r.getRequestURI().getPath().split("/");
+		if (splitUrl.length != 3) { //IDK why its 3 and not 2 but i honestly dont care
+			this.sendStatus(r, 400);
+			return;
+		}
+
+        try{
+
+            //Get the request body 
+            String body = Utils.convert(r.getRequestBody());
+            JSONObject deserialized = new JSONObject(body);
+            String name = null;
+            String email = null; 
+            String password = null; //initialize to null cause i'm a good little programmer
+
+            //Validate and check the stuff in body
+            //Listen ik i didn't check the name like i did email and password but these ifs are long enough man 
+            String[] field = {"name", "email", "password"};
+            Class<?>[] bodyTypes = {String.class, String.class};
+            if (validateFields(deserialized, field, bodyTypes)) {
+                if (deserialized.has("email") && deserialized.has("password")){
+                    if(deserialized.get("email").getClass() == String.class 
+                    && deserialized.get("password").getClass() == String.class){
+                        name = deserialized.getString("name");
+                        email = deserialized.getString("email");
+                        password = deserialized.getString("password");
+                    }
+                }
+            }  else {//400 if any fields were wrong or validation fails
+                this.sendStatus(r, 400);
+                return;
+            }
+
+            ResultSet result;
+            boolean resultHasNext;
+            result = this.dao.getUserDataFromEmail(email);
+            resultHasNext = result.next();
+            if (!resultHasNext) {
+                int uid = this.dao.registerUser(name, email, password);
+                // making the response
+                JSONObject resp = new JSONObject();
+                resp.put("uid", String.valueOf(uid));
+                this.sendResponse(r, resp, 200); //200 on success
+            } else {
+                this.sendStatus(r, 409);
+                return;
+            }
+            
+        } catch (Exception e){ 
+            e.printStackTrace();
+              this.sendStatus(r, 500);
+              return;
+          }
+
+
+
+
+
+
     }
 }
