f92aee2dbdc39fb37efa5cefb1fbdf61db70bfb9
Nicole Droi <nicoled0210@hotmail.com>
2022-11-23 22:53:27 -0500

    test/tripInfoAppTest added my portion of the tests

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -21,7 +21,7 @@ public class MongoDao {
 	public MongoCollection<Document> collection;
 	private final String username = "root";
 	private final String password = "123456";
-	private final String dbName = "todos"; //not too sure what this should be yet
+	private final String dbName = "trip"; //not too sure what this should be yet
 
 	public MongoDao() {
 		Dotenv dotenv = Dotenv.load();
